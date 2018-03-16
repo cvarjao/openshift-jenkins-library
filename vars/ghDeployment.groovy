8416cfd88fece56468cc5ae9c00a4f19ee47de4c
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-15 23:41:08 -0700

    Update ghDeployment.groovy

--- vars/ghDeployment.groovy
+++ vars/ghDeployment.groovy
@@ -1,17 +1,19 @@
 import org.kohsuke.github.*;
 
-@NonCPS
+//@NonCPS
 def call(String environmentName = 'PREVIEW') {
   def gitRepoUrl = scm.getUserRemoteConfigs()[0].getUrl()
   def gitRepoFullName=gitRepoUrl.replace('https://github.com/', '').replace('.git', '')
   echo "gitRepoFullName='${gitRepoFullName}'"
   
-  withCredentials([usernamePassword(credentialsId: 'github-account', passwordVariable: 'githubPassword', usernameVariable: 'githubUsername')]) {
+  //withCredentials([usernamePassword(credentialsId: 'github-account', passwordVariable: 'githubPassword', usernameVariable: 'githubUsername')]) {
+     def githubUsername=env.GH_USERNAME;
+     def githubPassword = env.GH_PASSWORD;
       def github=new GitHubBuilder().withPassword(githubUsername, githubPassword).build()
       def ghRepo=github.getRepository(gitRepoFullName);
       def ghDeploymentResponse=ghRepo.createDeployment(gitCommitId).environment(environmentName).description("Preview deployment").requiredContexts([]).create();
       ghDeploymentResponse.createStatus(GHDeploymentState.SUCCESS).targetUrl("http://somewhere.here.com").description("Preview deplyment2").create();
       ghDeploymentResponse = null;
-  }
+  //}
   
 }
