1cabbc72dfbfe928bb0922a5dd0f0d2b61375ccc
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-19 20:19:23 -0700

    Update openShiftApplyDeploymentConfig.groovy

--- vars/openShiftApplyDeploymentConfig.groovy
+++ vars/openShiftApplyDeploymentConfig.groovy
@@ -32,7 +32,7 @@ def updateContainerImages(_openshift, containers, triggers) {
     }
 }
 
-def call(_openshift, String appName, String envName, List models) {
+def call(_openshift, String appName, String envName, List models, buildImageStreams) {
     def dcSelector=['app-name':appName, 'env-name':envName];
     for ( m in models ) {
       if ("DeploymentConfig".equals(m.kind)){
