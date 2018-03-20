64e5da88544b317a2995268f08de2f7cdf04d26e
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-19 23:22:15 -0700

    Update openShiftApplyBuildConfig.groovy

--- vars/openShiftApplyBuildConfig.groovy
+++ vars/openShiftApplyBuildConfig.groovy
@@ -1,5 +1,22 @@
 
 def call(_openshift, String appName, String envName, List models) {
+  def bcSelector=['app-name':appName, 'env-name':buildEnvName];
+  
+  echo "Waiting for all pending builds to complete or cancel"
+  _openshift.selector( 'bc', bcSelector).cancelBuild();
+
+  _openshift.selector( 'builds', bcSelector).watch {
+      if ( it.count() == 0 ) return true
+      def allDone = true
+      it.withEach {
+          def buildModel = it.object()
+          if ( it.object().status.phase != "Complete" &&  it.object().status.phase != "Failed") {
+              allDone = false
+          }
+      }
+      return allDone;
+  }
+  
   echo "Applying ${models.size()} objects for '${appName}' for '${envName}'"
   for ( o in models ) {
      echo "Processing '${o.kind}/${o.metadata.name}'"
