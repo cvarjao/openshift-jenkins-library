37fd892e62773a758dab4fc3ef74907a8adb51a5
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 02:01:38 -0700

    Update openShiftDeploy.groovy

--- vars/openShiftDeploy.groovy
+++ vars/openShiftDeploy.groovy
@@ -38,8 +38,8 @@ def call(metadata, Closure body) {
             models = context.models();
           }
           
-            echo "${models}"
-          //openShiftApplyDeploymentConfig(openshift, buildProjectName, metadata.appName, envName, models, buildImageStreams)
+          echo "${models}"
+          openShiftApplyDeploymentConfig(openshift, buildProjectName, metadata.appName, envName, models, buildImageStreams)
 
           } // end openshift.withProject()
       } // end openshift.withCredentials()
