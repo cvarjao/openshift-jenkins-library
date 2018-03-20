1dab003626c81cdae4dc22ffde6677edaba46dac
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 01:21:37 -0700

    Update openShiftBuild.groovy

--- vars/openShiftBuild.groovy
+++ vars/openShiftBuild.groovy
@@ -11,11 +11,20 @@ def call(metadata, Closure body) {
   openshift.withCluster() {
     echo "project:${openshift.project()}"
     echo "models:${context.dump()}"
+    def models=[];
+    
     if (context.models!=null){
       context.models.resolveStrategy = Closure.DELEGATE_FIRST
       context.models.delegate = this
-      echo "models:${context.models()}"
+      models = context.models();
     }
     
+    echo 'Processing template ...'
+    openShiftApplyBuildConfig(openshift, metadata.appName, metadata.buildEnvName, models)
+
+    echo 'Creating/Updating Objects (from template)'
+    def builds=[];
+    builds.add(openShiftStartBuild(openshift, ['app-name':metadata.appName, 'env-name':metadata.buildEnvName], "${metadata.modules['spring-petclinic'].commit}"));
+    openShiftWaitForBuilds(openshift, builds)
   }
 }
