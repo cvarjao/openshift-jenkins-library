f2c7b583558cbe42b41087cca016b4fc41b18c99
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 00:48:38 -0700

    Update standardDeliveryPipeline.groovy

--- vars/standardDeliveryPipeline.groovy
+++ vars/standardDeliveryPipeline.groovy
@@ -55,13 +55,13 @@ def call(body) {
                 steps {
                     script {
                         openShiftBuild(metadata, {
-                            models = {(openshift.process("-f", "openshift.bc.json",
+                            models {(openshift.process("-f", "openshift.bc.json",
                                     "-p", "APP_NAME=${metadata.appName}",
                                     "-p", "ENV_NAME=${metadata.buildEnvName}",
                                     "-p", "NAME_PREFIX=${metadata.buildNamePrefix}",
                                     "-p", "NAME_SUFFIX=${metadata.buildNameSuffix}",
                                                          "-p", "GIT_REPO_URL=${metadata.gitRepoUrl}"))
-                                     }
+                            }
                         });
                         
                         openshift.withCluster() {
