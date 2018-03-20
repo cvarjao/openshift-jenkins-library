1091276fe12599df6f2613b9c61bb56d1ad318bd
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 00:39:07 -0700

    Update openShiftBuild.groovy

--- vars/openShiftBuild.groovy
+++ vars/openShiftBuild.groovy
@@ -1,5 +1,13 @@
 
-def call(metadata) {
+def call(metadata, Closure body) {
+  def context= ['models':[]]
+  
+  if (body!=null){
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = context
+    body()
+  }
+  
   openshift.withCluster() {
     echo "project:${openshift.project()}"
   }
