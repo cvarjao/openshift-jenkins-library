f9e1c3181180d505de8ed8fa37053cb2de317b66
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 20:33:59 -0700

    Update OpenShiftHelper.groovy

--- src/bcgov/OpenShiftHelper.groovy
+++ src/bcgov/OpenShiftHelper.groovy
@@ -1,10 +1,13 @@
 package bcgov;
 
+import org.jenkinsci.plugins.workflow.cps.CpsScript;
+
 class OpenShiftHelper {
-  static def build(script, String appName, String envName, Closure models) {
-    null;
+  static def build(CpsScript script, String appName, String envName, Closure models) {
+    script.echo "OpenShiftHelper.build: Hello"
   }
   static def applyBuildConfig(script, String appName, String envName, Closure models) {
+    script.echo "OpenShiftHelper.applyBuildConfig: Hello"
     //def bcSelector=['app-name':appName, 'env-name':envName];
     //echo "Cancelling all pending builds"
     //openshift.selector( 'bc', bcSelector).cancelBuild();
