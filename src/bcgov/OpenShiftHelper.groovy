6ce8abbe32ee2e192ff27123ce00a71a45272f8f
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-03-20 22:10:24 -0700

    asg

--- src/bcgov/OpenShiftHelper.groovy
+++ src/bcgov/OpenShiftHelper.groovy
@@ -5,15 +5,17 @@ import org.jenkinsci.plugins.workflow.cps.CpsScript;
 class OpenShiftHelper {
 
     def build(CpsScript script, Map __context) {
-        script.echo "openShiftBuild:openshift1:${script.openshift.dump()}"
-        script.openshift.withCluster() {
-            script.echo "openShiftBuild:openshift2:${script.openshift.dump()}"
-            script.openshift.withProject(script.openshift.project()) {
+        def openshift=script.openshift;
+
+        script.echo "openShiftBuild:openshift1:${openshift.dump()}"
+        openshift.withCluster() {
+            script.echo "openShiftBuild:openshift2:${openshift.dump()}"
+            openshift.withProject(openshift.project()) {
                 def models = [];
                 def metadata = __context.metadata;
 
-                script.echo "openShiftBuild:openshift3:${script.openshift.dump()}"
-                script.echo "openShiftBuild: project:${script.openshift.project()}"
+                script.echo "openShiftBuild:openshift3:${openshift.dump()}"
+                script.echo "openShiftBuild: project:${openshift.project()}"
 
                 if (__context.models != null) {
                     __context.models.resolveStrategy = Closure.DELEGATE_FIRST;
@@ -26,31 +28,31 @@ class OpenShiftHelper {
 
                 script.echo 'Processing template ...'
 
-                applyBuildConfig(script, metadata.appName, metadata.buildEnvName, models);
+                applyBuildConfig(script, openshift, metadata.appName, metadata.buildEnvName, models);
 
                 script.echo 'Creating/Updating Objects (from template)'
                 def builds = [];
-                builds.add(startBuild(script, ['app-name': metadata.appName, 'env-name': metadata.buildEnvName], "${metadata.modules['spring-petclinic'].commit}"));
-                waitForBuilds(script, builds)
+                builds.add(startBuild(script, openshift, ['app-name': metadata.appName, 'env-name': metadata.buildEnvName], "${metadata.modules['spring-petclinic'].commit}"));
+                waitForBuilds(script, openshift, builds)
             }
         }
     }
 
-    def applyBuildConfig(CpsScript script, appName, envName, models) {
+    def applyBuildConfig(CpsScript script, openshift, appName, envName, models) {
         //def body = {
         script.echo "OpenShiftHelper.applyBuildConfig: Hello - ${script.dump()}"
-        script.echo "openShiftBuild:openshift2:${script.openshift.dump()}"
+        script.echo "openShiftBuild:openshift2:${openshift.dump()}"
         //} //end 'body 'closure
 
         def bcSelector = ['app-name': appName, 'env-name': envName];
 
-        script.echo "openShiftApplyBuildConfig:openshift1:${script.openshift.dump()}"
+        script.echo "openShiftApplyBuildConfig:openshift1:${openshift.dump()}"
 
         script.echo "Cancelling all pending builds"
-        script.openshift.selector('bc', bcSelector).cancelBuild();
+        openshift.selector('bc', bcSelector).cancelBuild();
 
         script.echo "Waiting for all pending builds to complete or cancel"
-        script.openshift.selector('builds', bcSelector).watch {
+        openshift.selector('builds', bcSelector).watch {
             if (it.count() == 0) return true
             def allDone = true
             it.withEach {
@@ -77,7 +79,7 @@ class OpenShiftHelper {
         }
         */
         }
-        script.openshift.apply(models);
+        openshift.apply(models);
 
         //body.resolveStrategy = Closure.DELEGATE_FIRST;
         //body.delegate = script;
@@ -89,13 +91,13 @@ class OpenShiftHelper {
     }
 
 
-    def startBuild(CpsScript script, baseSelector, commitId) {
+    def startBuild(CpsScript script, openshift, baseSelector, commitId) {
         String buildNameSelector = null;
 
-        def buildSelector = script.openshift.selector('builds', baseSelector + ['commit-id': "${commitId}"]);
+        def buildSelector = openshift.selector('builds', baseSelector + ['commit-id': "${commitId}"]);
         if (buildSelector.count() == 0) {
             script.echo "Starting new build for '${baseSelector}'"
-            buildSelector = script.openshift.selector('bc', baseSelector).startBuild("--commit=${commitId}")
+            buildSelector = openshift.selector('bc', baseSelector).startBuild("--commit=${commitId}")
             script.echo "New build started - ${buildSelector.name()}"
             buildSelector.label(['commit-id': "${commitId}"], "--overwrite")
             buildNameSelector = buildSelector.name()
@@ -123,9 +125,9 @@ class OpenShiftHelper {
     }
 
 
-    def waitForBuilds(CpsScript script, List builds) {
+    def waitForBuilds(CpsScript script, openshift, List builds) {
         //Wait for all builds to complete
-        script.openshift.selector(builds).watch {
+        openshift.selector(builds).watch {
             def build = it.object();
             def buildDone = ("Complete".equalsIgnoreCase(build.status.phase) || "Cancelled".equalsIgnoreCase(build.status.phase))
             if (!buildDone) {
@@ -134,7 +136,7 @@ class OpenShiftHelper {
             return buildDone;
         }
 
-        script.openshift.selector(builds).withEach { build ->
+        openshift.selector(builds).withEach { build ->
             def bo = build.object(); // build object
             if (!"Complete".equalsIgnoreCase(bo.status.phase)) {
                 error "Build '${build.name()}' did not successfully complete (${bo.status.phase})"
