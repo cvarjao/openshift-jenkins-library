53190c66caae45f2826bfeeece2ed60cb6b77d7d
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2018-03-20 18:37:53 -0700

    Update openShiftBuild.groovy

--- vars/openShiftBuild.groovy
+++ vars/openShiftBuild.groovy
@@ -12,19 +12,20 @@ def call(metadata, Closure body) {
     echo "project:${openshift.project()}"
     echo "models:${context.dump()}"
     def models=[];
-    
-    if (context.models!=null){
-      context.models.resolveStrategy = Closure.DELEGATE_FIRST
-      context.models.delegate = this
-      models = context.models();
-    }
-    
-    echo 'Processing template ...'
-    openShiftApplyBuildConfig(openshift, metadata.appName, metadata.buildEnvName, models)
+    openshift.withProject(openshift.project()) {
+      if (context.models!=null){
+        context.models.resolveStrategy = Closure.DELEGATE_FIRST
+        context.models.delegate = this
+        models = context.models();
+      }
 
-    echo 'Creating/Updating Objects (from template)'
-    def builds=[];
-    builds.add(openShiftStartBuild(openshift, ['app-name':metadata.appName, 'env-name':metadata.buildEnvName], "${metadata.modules['spring-petclinic'].commit}"));
-    openShiftWaitForBuilds(openshift, builds)
+      echo 'Processing template ...'
+      openShiftApplyBuildConfig(openshift, metadata.appName, metadata.buildEnvName, models)
+
+      echo 'Creating/Updating Objects (from template)'
+      def builds=[];
+      builds.add(openShiftStartBuild(openshift, ['app-name':metadata.appName, 'env-name':metadata.buildEnvName], "${metadata.modules['spring-petclinic'].commit}"));
+      openShiftWaitForBuilds(openshift, builds)
+    }
   }
 }
