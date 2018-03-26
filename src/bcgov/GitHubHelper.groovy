d0141db5b529c815d9d394ceefef1e21659b494d
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-03-26 00:38:24 -0700

    asf

--- src/bcgov/GitHubHelper.groovy
+++ src/bcgov/GitHubHelper.groovy
@@ -45,7 +45,7 @@ class GitHubHelper {
         long deploymentId = -1
         GHDeploymentBuilder builder=getGitHubRepository(url).createDeployment(ref)
         builder.environment(deploymentConfig.environment)
-        deploymentId = builder.create().getId()
+        builder.create()
         //deployment=null
         /*
         if (deploymentConfig!=null) {
