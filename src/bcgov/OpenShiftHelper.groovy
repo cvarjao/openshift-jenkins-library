40bdfbca2365ab1c842a33e7d1910a95f0ceff79
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-04-01 22:19:58 -0700

    asdasd

--- src/bcgov/OpenShiftHelper.groovy
+++ src/bcgov/OpenShiftHelper.groovy
@@ -288,8 +288,8 @@ class OpenShiftHelper {
                     boolean deployProjectAccessible = accessibleProjects.contains(deployProjectName)
                     boolean envProjectAccessible = accessibleProjects.contains(envProjectName)
 
-                    script.echo "deployProjectName:'${deployProjectName}' accessible:${deployProjectAccessible}"
-                    script.echo "envProjectName:'${envProjectName}' accessible:${envProjectAccessible}"
+                    //script.echo "deployProjectName:'${deployProjectName}' accessible:${deployProjectAccessible}"
+                    //script.echo "envProjectName:'${envProjectName}' accessible:${envProjectAccessible}"
 
                     if (envProjectAccessible){
                         modifiedEnvProjects[envKeyName]=envProjectName
@@ -325,7 +325,7 @@ class OpenShiftHelper {
             script.echo "Setting target project for '${envKeyName}' to '${modifiedEnvProjects[envKeyName]}' "
             context.env[envKeyName].project=modifiedEnvProjects[envKeyName]
         }
-        script.error "stop here"
+        //script.error "stop here"
     }
 
     def prepareForCD(CpsScript script, Map context) {
